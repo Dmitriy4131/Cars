5d269464ac71d3a02baf5e9980995e533a9db8a4
Dmitriy4131 <127550671+Dmitriy4131@users.noreply.github.com>
2023-03-11 15:27:07 +0300

    Add files via upload

---
+++ src/main/java/ru/rak/cars/Car.java
@@ -0,0 +1,82 @@
+package ru.rak.cars;
+
+import ru.rak.details.Engine;
+import ru.rak.professions.Driver;
+
+import java.util.Scanner;
+
+public class Car {
+    private String carClass, brand;
+    public Engine engine;
+    public Driver driver;
+
+    public Car(String carClass, String brand, Engine engine, Driver driver) {
+        this.carClass = carClass;
+        this.brand = brand;
+        this.engine = engine;
+        this.driver = driver;
+    }
+
+    public String getCarClass() {
+        return carClass;
+    }
+
+    public void setCarClass(String carClass) {
+        this.carClass = carClass;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public static Car addCarToListCars(Scanner scanner) {
+        Car car = new Car(" ", " ", new Engine(0, ""),
+                new Driver(" ", " ", 0, 0));
+        Scanner sc = new Scanner(System.in);
+        System.out.print("CarClass ");
+        String carClass = sc.next();
+        car.setCarClass(carClass);
+        System.out.print("Brand ");
+        String brand = sc.next();
+        car.setBrand(brand);
+
+        System.out.print("Power ");
+        int power = sc.nextInt();
+        car.engine.setPower(power);
+        System.out.print("Company ");
+        String company = sc.next();
+        car.engine.setCompany(company);
+
+        System.out.print("FirstName ");
+        String firstName = sc.next();
+        car.driver.setFirstName(firstName);
+        System.out.print("LastName ");
+        String lastName = sc.next();
+        car.driver.setLastName(lastName);
+        System.out.print("age ");
+        int age = sc.nextInt();
+        car.driver.setAge(age);
+        System.out.print("Experience ");
+        int experience = sc.nextInt();
+        car.driver.setExperience(experience);
+        return car;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "carClass='" + getCarClass() + '\'' +
+                ", brand='" + getBrand() + '\'' +
+                ", power=" + engine.getPower() +
+                ", company='" + engine.getCompany() + '\'' +
+                ", firstName='" + driver.getFirstName() + '\'' +
+                ", lastName='" + driver.getLastName() + '\'' +
+                ", age=" + driver.getAge() +
+                ", experience=" + driver.getExperience() +
+                "}\n";
+    }
+}
